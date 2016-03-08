e20dcb2cbe7c15d13edc5a0895f58f9adf5fed15
Quan Nguyen <quanmn.libra@gmail.com>
2016-03-08 22:48:46 +0700

    Home Screen

---
+++ src/components/common/Heading.js
@@ -0,0 +1,83 @@
+'use strict';
+
+import React, {
+    Component,
+    Text
+} from 'react-native';
+
+const TextStylePropTypes = require('TextStylePropTypes');
+const StyleSheetPropType = require('StyleSheetPropType');
+const ViewStylePropTypes = require('ViewStylePropTypes');
+const stylePropType = StyleSheetPropType(ViewStylePropTypes);
+const labelStylePropType = StyleSheetPropType(TextStylePropTypes);
+
+class BaseHeading extends Component {
+
+  static propTypes = {
+    styles: stylePropType,
+    labelStyles: labelStylePropType,
+    fontSize: React.PropTypes.number
+  }
+
+  constructor(props) {
+    super(props);
+    if (isNaN(props.fontSize)) {
+      this.fontSize = 8;
+    }
+  }
+
+  render() {
+    var labelStyles = [{fontSize: this.fontSize, fontWeight: 'bold', color: '#FFFFFF'}];
+    if (this.props.labelStyles) {
+      labelStyles.push(this.props.labelStyles);
+    }
+    return (
+          <Text style={labelStyles}>
+            {this.props.children}
+          </Text>
+    );
+  }
+
+}
+
+class H1 extends BaseHeading {
+
+  constructor(props) {
+    super(props);
+    this.fontSize = 32;
+  }
+
+}
+
+class H2 extends BaseHeading {
+  constructor(props) {
+    super(props);
+    this.fontSize = 24;
+  }
+
+}
+
+class H3 extends BaseHeading {
+
+  constructor(props) {
+    super(props);
+    this.fontSize = 18;
+  }
+
+}
+
+class H5 extends BaseHeading {
+
+  constructor(props) {
+    super(props);
+    this.fontSize = 13;
+  }
+
+}
+
+module.exports = {
+  H1: H1,
+  H2: H2,
+  H3: H3,
+  H5: H5
+};
--- src/components/screens/home/Home.js
+++ src/components/screens/home/Home.js
@@ -4,10 +4,15 @@ const {
   View,
   StyleSheet,
   Image,
-  Text
+  Text,
+  TouchableOpacity
 } = React;
 
 import bg from '../../img/background.jpg';
+import arrow from '../../img/arrow64.png';
+import smallArrow from '../../img/right-arrow.png';
+import { H1 } from './../../common/Heading';
+import Dimensions from 'Dimensions';
 
 const styles = StyleSheet.create({
   container: {
@@ -16,17 +21,179 @@ const styles = StyleSheet.create({
   heading: {
     flex: 1,
     position: 'absolute',
-    top: 20
+    top: 10,
+    left: 0,
+    width: Dimensions.get('window').width / 1.2,
+    alignItems: 'center'
+  },
+  headingTitle: {
+    color: '#FB8C00',
+    textAlign: 'center',
+    fontSize: 28
+  },
+  checkWeight: {
+    flex: 1,
+    position: 'absolute',
+    top: 120,
+    left: 50,
+    width: 120,
+    height: 50,
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#81C784',
+    backgroundColor: '#81C784',
+    borderRadius: 10,
+    padding: 8
+  },
+  checkWeightTitle: {
+    fontWeight: 'bold',
+    color: '#FFFFFF'
+  },
+  circleButton: {
+    flex: 1,
+    position: 'absolute',
+    top: 30,
+    right: 130,
+    width: 100,
+    height: 100,
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#FB8C00',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 50,
+    paddingTop: 35,
+    paddingBottom: 40
+  },
+  btnExit: {
+    flex: 1,
+    position: 'absolute',
+    right: 5,
+    top: 10
+  },
+  btnExitText: {
+    top: -40,
+    left: 10,
+    color: '#FFFFFF'
+  },
+  smallArrow1: {
+    flex: 1,
+    position: 'absolute',
+    right: 120,
+    top: 130,
+    transform: [{rotate: '45deg'}]
+  },
+  smallArrow2: {
+    flex: 1,
+    position: 'absolute',
+    right: 170,
+    top: 200,
+    transform: [{rotate: '180deg'}]
+  },
+  smallArrow3: {
+    flex: 1,
+    position: 'absolute',
+    right: 210,
+    top: 130,
+    transform: [{rotate: '135deg'}]
   }
 });
 
 class Home extends Component {
+  _onPressBtnCheckWeight() {
+    console.log('_onPressBtnCheckWeight');
+  }
+
+  _onPressBtnCheckQty() {
+    console.log('_onPressBtnCheckQty');
+  }
+
+  _onPressBtnAddItem() {
+    console.log('_onPressBtnAddItem');
+  }
+
+  _onPressBtnSellItem() {
+    console.log('_onPressBtnSellItem');
+  }
+
+  _onPressBtnExit() {
+    console.log('_onPressBtnExit');
+  }
+
+  _renderHeading() {
+    return (
+      <View style={styles.heading}>
+        <H1 labelStyles={styles.headingTitle}>Phần mềm quản</H1>
+        <H1 labelStyles={styles.headingTitle}>lý</H1>
+        <H1 labelStyles={styles.headingTitle}>Sản phẩm PVC</H1>
+      </View>
+    );
+  }
+
+  _renderCheckWeight() {
+    return (
+      <TouchableOpacity style={styles.checkWeight} onPress={this._onPressBtnCheckQty.bind(this)}>
+        <View>
+          <Text style={[styles.checkWeightTitle, {marginLeft: 10}]}>KIỂM TRA</Text>
+          <View style={{flex: 1}}/>
+          <Text style={styles.checkWeightTitle}>KHỐI LƯỢNG</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+  _renderCheckQty() {
+    return (
+      <TouchableOpacity style={styles.circleButton} onPress={this._onPressBtnCheckWeight.bind(this)}>
+        <Text>KIỂM TRA SỐ</Text>
+        <View style={{flex: 1}}/>
+        <Text>LƯỢNG</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  _renderAddItem() {
+    return (
+      <TouchableOpacity style={[styles.circleButton, {top: 160, right: 220}]} onPress={this._onPressBtnAddItem.bind(this)}>
+        <Text style={{marginTop: 5}}>NHẬP HÀNG</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  _renderSellItem() {
+    return (
+      <TouchableOpacity style={[styles.circleButton, {top: 160, right: 40}]} onPress={this._onPressBtnSellItem.bind(this)}>
+        <Text style={{marginTop: 5}}>BÁN HÀNG</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  _renderButtonExit() {
+    return (
+      <TouchableOpacity style={styles.btnExit} onPress={this._onPressBtnExit.bind(this)}>
+        <Image source={arrow} resizeMode={Image.resizeMode.contain}/>
+        <Text style={styles.btnExitText}>Thoát</Text>
+      </TouchableOpacity>
+    );
+  }
+
   render() {
     return (
         <View style={styles.container}>
           <Image source={bg} resizeMode={Image.resizeMode.contain}/>
-          <View style={styles.heading}>
-            <Text>PVC Management</Text>
+          {this._renderHeading()}
+          {this._renderCheckQty()}
+          {this._renderAddItem()}
+          {this._renderSellItem()}
+          {this._renderCheckWeight()}
+          {this._renderButtonExit()}
+          <View style={styles.smallArrow1}>
+            <Image source={smallArrow} resizeMode={Image.resizeMode.contain}/>
+          </View>
+          <View style={styles.smallArrow2}>
+            <Image source={smallArrow} resizeMode={Image.resizeMode.contain}/>
+          </View>
+          <View style={styles.smallArrow3}>
+            <Image source={smallArrow} resizeMode={Image.resizeMode.contain}/>
           </View>
         </View>
     );
